88cd46a48e7b69557d2903c0f036220c3f5058ba
okan <okan28001@gmail.com>
2025-02-16 17:23:04 +0300

    Set up react router

---
+++ registry/src/App.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import SearchPage from "./pages/SearchPage";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      {
+        path: "/packages/:name",
+        element: <DetailsPage />,
+      },
+    ],
+  },
+]);
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ registry/src/pages/Root.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/header";
+export default function Root() {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+}
