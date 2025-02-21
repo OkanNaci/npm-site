5d0fb925fac041eaed49a2b15cacb567e55b94f1
okan <okan28001@gmail.com>
2025-02-21 17:48:12 +0300

    Search Page Done

--- registry/src/pages/Root.tsx
+++ registry/src/pages/Root.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/header";
 export default function Root() {
   return (
-    <div>
+    <div className="container mx-auto px-20 ">
       <Header />
       <Outlet />
     </div>
