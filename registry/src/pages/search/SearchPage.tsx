279148f001bde939788321b76c543655ee7da58e
okan <okan28001@gmail.com>
2025-02-21 16:57:40 +0300

    Add Type safety

--- registry/src/pages/search/SearchPage.tsx
+++ registry/src/pages/search/SearchPage.tsx
@@ -1,6 +1,7 @@
+import type { SearchLoaderResult } from "./searchLoader";
 import { useLoaderData } from "react-router-dom";
 export default function SearchPage() {
-  const data = useLoaderData();
+  const data = useLoaderData() as SearchLoaderResult;
   console.log(data);
   return <div>Search Page</div>;
 }
